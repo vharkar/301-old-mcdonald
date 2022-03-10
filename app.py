1a51931eba8bed9e6471d95eb3c03e3ea43376a1
vharikar <vibha_harikar@intuit.com>
2022-03-10 01:46:29 +0000

    Changed to drop down

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
 mycolumn='corn'
-myheading1 = f"Wow! That's a lot of {mycolumn}!"
+myheading1 = f"Lesson4 - Draw with plotly"
 mygraphtitle = '2011 US Agriculture Exports by State'
 mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
 mycolorbartitle = "Millions USD"
-tabtitle = 'Old McDonald'
+tabtitle = 'US Agriculture'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
 githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
 
@@ -25,21 +25,6 @@ githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
 
 import pandas as pd
 df = pd.read_csv('assets/usa-2011-agriculture.csv')
-
-fig = go.Figure(data=go.Choropleth(
-    locations=df['code'], # Spatial coordinates
-    z = df[mycolumn].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = mycolorscale,
-    colorbar_title = mycolorbartitle,
-))
-
-fig.update_layout(
-    title_text = mygraphtitle,
-    geo_scope='usa',
-    width=1200,
-    height=800
-)
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -51,6 +36,16 @@ app.title=tabtitle
 
 app.layout = html.Div(children=[
     html.H1(myheading1),
+    dcc.Dropdown(
+    id='your_input_here',
+        options=[
+                {'label':list_of_columns[0], 'value':list_of_columns[0]},
+                {'label':list_of_columns[1], 'value':list_of_columns[1]},
+                {'label':list_of_columns[2], 'value':list_of_columns[2]},
+                {'label':list_of_columns[3], 'value':list_of_columns[3]},
+                ],
+        value=list_of_columns[0],
+    ),
     dcc.Graph(
         id='figure-1',
         figure=fig
@@ -61,6 +56,26 @@ app.layout = html.Div(children=[
     ]
 )
 
+########## Define Callback
+@app.callback(Output('figure-1', 'figure'),
+              [Input('your_input_here', 'value')])
+def draw_map(mycolumn):
+    fig = go.Figure(data=go.Choropleth(
+      locations=df['code'], # Spatial coordinates
+      z = df[mycolumn].astype(float), # Data to be color-coded
+      locationmode = 'USA-states', # set of locations match entries in `locations`
+      colorscale = mycolorscale,
+      colorbar_title = mycolorbartitle,
+    ))
+
+    fig.update_layout(
+      title_text = mygraphtitle,
+      geo_scope='usa',
+      width=1200,
+      height=800
+    )
+    return fig
+
 ############ Deploy
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=TRUE)
